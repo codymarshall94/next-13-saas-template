8757e303ecfcb2dcfc06a57387febb2699e4be78
Cody Marshall <codymstuntsx@gmail.com>
2023-10-24 20:51:02 -0700

    created: root page meta tag

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js SaaS Starter",
+  description: "SaaS dashboard fast jumpstart. Styled with shadcn.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+};
